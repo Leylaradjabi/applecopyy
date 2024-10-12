9baa86bb6612e67da05f790a623897c5c225b6da
Leylaradjabi <radlejla5@gmail.com>
2024-10-12 15:42:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let button = document.querySelector('.btn')
+let closeButton = document.querySelector('.closebtn')
+let card = document.querySelector('.mod')
+
+
+button.onclick = () => {
+    card.classList.toggle('show')
+    console.log(card);
+}   
+    closeButton.onclick = () => {
+    card.classList.remove('show');
+    console.log(card);
+};
+
+
